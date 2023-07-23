6f918a4f4af365452ad87b48c05ac9bcb09162e4
Heesoo <hilarious_u@sookmyung.ac.kr>
2023-07-23 17:29:10 +0900

    MovieList.jsx

--- FE_4th/week04/src/App.js
+++ FE_4th/week04/src/App.js
@@ -1,12 +1,15 @@
+import React from 'react';
 import './App.css';
 import Hello from './Hello';
 import Counter from './Counter';
+import MovieList from './MovieList';
 
 function App() {
   return (
     <>
     <Hello name="Heesoo" color="blue"/>
     <Counter/>
+    <MovieList />
     </>
   );
 }
